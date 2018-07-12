caa7942840423df8dfbbfbc218a9c70f5b3555ed
沈建芳 <crabfang@163.com>
2018-07-12 20:35:10 +0800

    [MDF]完善阅读界面逻辑

--- app/src/main/java/com/cabe/app/novel/widget/NovelContentView.java
+++ app/src/main/java/com/cabe/app/novel/widget/NovelContentView.java
@@ -3,6 +3,7 @@ package com.cabe.app.novel.widget;
 import android.content.Context;
 import android.text.Html;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -18,22 +19,40 @@ import com.cabe.lib.cache.interactor.ViewPresenter;
  * 作者：沈建芳 on 2018/7/12 19:27
  */
 public class NovelContentView extends LinearLayout {
+    private View viewMain;
     private TextView tvTitle;
     private TextView tvContent;
+    private View loading;
+    private View viewError;
+
+    private NovelContent novel;
     public NovelContentView(Context context) {
         super(context);
         LayoutInflater.from(context).inflate(R.layout.item_novel_content, this, true);
+        viewMain = findViewById(R.id.fragment_novel_content_main);
         tvTitle = findViewById(R.id.fragment_novel_content_title);
         tvContent = findViewById(R.id.fragment_novel_content_val);
+        loading = findViewById(R.id.fragment_novel_content_loading);
+        viewError = findViewById(R.id.fragment_novel_content_error);
+        View btnRetry = findViewById(R.id.fragment_novel_content_error_btn);
+        btnRetry.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadContent(novel);
+            }
+        });
     }
 
     public void loadContent(final NovelContent novel) {
+        this.novel = novel;
         if(novel == null) return;
 
+        loading.setVisibility(View.VISIBLE);
         ViewPresenter<NovelContent> presenter = new ViewPresenter<NovelContent>() {
             @Override
             public void error(CacheSource from, int code, String info) {
-
+                viewError.setVisibility(View.VISIBLE);
+                viewMain.setVisibility(View.GONE);
             }
             @Override
             public void load(CacheSource from, NovelContent content) {
@@ -44,9 +63,12 @@ public class NovelContentView extends LinearLayout {
                 cacheContent.nextUrl = content.nextUrl;
                 cacheContent.source = novel.source;
                 updateView(content);
+                viewError.setVisibility(View.GONE);
+                viewMain.setVisibility(View.VISIBLE);
             }
             @Override
             public void complete(CacheSource from) {
+                loading.setVisibility(View.GONE);
             }
         };
         String url = novel.url;
