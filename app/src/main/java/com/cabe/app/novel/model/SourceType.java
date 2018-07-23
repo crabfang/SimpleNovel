13fc0a34dd98e48c4132de6e5d107f6df69123e7
沈建芳 <crabfang@163.com>
2018-07-23 19:13:30 +0800

    [MDF]修复排行榜图片显示问题

--- app/src/main/java/com/cabe/app/novel/model/SourceType.java
+++ app/src/main/java/com/cabe/app/novel/model/SourceType.java
@@ -4,8 +4,8 @@ package com.cabe.app.novel.model;
  * 作者：沈建芳 on 2018/7/6 13:45
  */
 public enum SourceType {
-    X23US("顶点小说", "https://www.x23us.com", "https://www.x23us.com/modules/article/images/nocover.jpg"),
-    EKXS("2K小说", "https://www.2kxs.com", "https://www.2kxs.com/modules/article/images/nocover.jpg");
+    X23US("顶点小说", "https://www.x23us.com/", "https://www.x23us.com/modules/article/images/nocover.jpg"),
+    EKXS("2K小说", "https://www.2kxs.com/", "https://www.2kxs.com/modules/article/images/nocover.jpg");
     private String typeName;
     private String host;
     private String picUrl;
