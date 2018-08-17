f95ef3504b812cf887f9d2524f8fb8391f8400d5
沈建芳 <crabfang@163.com>
2018-08-17 16:52:16 +0800

    [MDF]调整缓存位置

--- app/src/main/java/com/cabe/app/novel/activity/MyApp.java
+++ app/src/main/java/com/cabe/app/novel/activity/MyApp.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import com.cabe.lib.cache.disk.DiskCacheManager;
 import com.pgyersdk.crash.PgyCrashManager;
 
+import java.io.File;
+
 /**
  * 作者：沈建芳 on 2017/10/11 11:17
  */
@@ -12,7 +14,12 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        DiskCacheManager.DISK_CACHE_PATH = getExternalCacheDir().toString();
+        File fileDir = getExternalFilesDir("configs");
+        if(fileDir != null) {
+            DiskCacheManager.DISK_CACHE_PATH = fileDir.getAbsolutePath();
+        } else {
+            DiskCacheManager.DISK_CACHE_PATH = getFilesDir().getAbsolutePath();
+        }
         PgyCrashManager.register();
     }
 }
