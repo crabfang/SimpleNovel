c03bde932868dece9336925c8cbeac30b55bf625
沈建芳 <shenjianfang@u51.com>
2022-11-11 16:17:37 +0800

    [MDF]移除DNS强转逻辑

--- app/src/main/java/com/cabe/app/novel/retrofit/OkHttpDns.kt
+++ app/src/main/java/com/cabe/app/novel/retrofit/OkHttpDns.kt
@@ -17,14 +17,18 @@ class OkHttpDns private constructor() : Dns {
     @Throws(UnknownHostException::class)
     override fun lookup(hostname: String): List<InetAddress> {
         //通过异步解析接口获取ip
-        val bqgHost = Uri.parse(SourceType.BQG.host).host
-        if (bqgHost == hostname) {
-            val ip = "23.225.140.157"
-            //如果ip不为null，直接使用该ip进行网络请求
-            val inetAddresses = listOf(*InetAddress.getAllByName(ip))
-            Log.e("OkHttpDns", "inetAddresses:$inetAddresses")
-            return inetAddresses
-        }
+//        val bqgHost = Uri.parse(SourceType.BQG.host).host
+//        if (bqgHost == hostname) {
+//            try {
+//                val ip = "23.225.140.157"
+//                //如果ip不为null，直接使用该ip进行网络请求
+//                val inetAddresses = listOf(*InetAddress.getAllByName(ip))
+//                Log.e("OkHttpDns", "inetAddresses:$inetAddresses")
+//                return inetAddresses
+//            } catch (e: Exception) {
+//                e.printStackTrace()
+//            }
+//        }
         //如果返回null，走系统DNS服务解析域名
         return Dns.SYSTEM.lookup(hostname)
     }
